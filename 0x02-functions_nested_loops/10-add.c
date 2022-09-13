c7d0bdaec6d513f8c93dc1885744049e4e040860
leeroysabs <leeroysabs@gmail.com>
2022-09-13 15:11:16 -0700

    a + b

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,7 +1,7 @@
-#nclude "main.h"
+#include "main.h"
 
 /**
- * aad - Adds two integers.
+ * add - Adds two integers.
  * @num1: The first integer to be added.
  * @num2: The second integer to be added.
  *
