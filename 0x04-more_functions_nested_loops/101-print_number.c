d7402f59210aa0356822255e95cc1b899e3cac9a
leeroysabs <leeroysabs@gmail.com>
2022-09-15 16:06:42 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,23 +1,22 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * print_number - Prints a number
+ * print_number - Prints numbers
  * @n: The number to print
+ * Return: void
  */
 
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int x;
 
+	x = n;
 	if (n < 0)
 	{
-		putchar('-');
-		num = -num;
+		_putchar(45);
+		x = -n;
 	}
-	if (num > 9)
-	{
-		print_number(num / 10);
-	}
-	putchar(num % 10 + '0');
+	if (x / 10)
+		print_number(x / 10);
+	_putchar(x % 10 + '0');
 }
