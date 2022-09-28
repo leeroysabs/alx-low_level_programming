9dc9ccc30407ebcf8c2c5a4dddf2238f25a0bfd3
leeroysabs <leeroysabs@gmail.com>
2022-09-28 09:48:10 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - calculates the length of a string
+ * @s: string to be used
+ *
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	int sum = 0;
+
+	if (*s != '\0')
+	{
+		sum++;
+		sum += _ strlen_recursion(s + 1);
+	}
+	return (sum);
+}
