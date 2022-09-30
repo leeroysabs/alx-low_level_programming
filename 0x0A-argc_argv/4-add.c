70a2eef5ef06029efbfe9a04a3c9a87a2293ec60
leeroysabs <leeroysabs@gmail.com>
2022-09-30 07:33:27 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,18 +13,15 @@
 int check_num(char *str)
 
 {
-	/*Declaring variables*/
 	unsigned int count;
 
 	count = 0;
-	while (count < strlen(str)) /*count string*/
-
+	while (count < strlen(str))
 	{
-		if (!isdigit(str[count])) /*check if str there are digit*/
+		if (!isdigit(str[count]))
 		{
 			return (0);
 		}
-
 		count++;
 	}
 	return (1);
